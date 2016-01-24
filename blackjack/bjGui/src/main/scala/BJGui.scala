877f3515da49d1895926478d9d128e73a043a50f
aneberle <aneberle@htwg-konstanz.de>
2016-01-24 21:37:10 +0100

    fixed bugs in the logic and made a nicer design for the UI

--- blackjack/bjGui/src/main/scala/BJGui.scala
+++ blackjack/bjGui/src/main/scala/BJGui.scala
@@ -11,8 +11,7 @@ import de.htwg.core.entities._
 object BJGui extends SimpleSwingApplication {
 
   def top = new MainFrame {
-    val game = GameCoreController.startNewGame
-    var round = GameCoreController.startNewRound(game)
+    var round = GameCoreController.startNewRound(GameCoreController.startNewGame)
 
     title = "Blackjack"
     // TODO in future: relative size to screen size
@@ -20,13 +19,16 @@ object BJGui extends SimpleSwingApplication {
 
     // Referenzen zu restlichen GUI Komponenten
     val bnGiveCard = new Button {
-      text = "Hit me!"
+      text = "Hit me"
       enabled = false
+      background = java.awt.Color.darkGray
+      foreground = java.awt.Color.white
     }
     val bnStand = new Button {
       text = "Stand"
       enabled = false
-      // TODO styling background = java.awt.Color.magenta
+      background = java.awt.Color.darkGray
+      foreground = java.awt.Color.white
     }
     val lblStake = new Label {
       text = "Stake in $: "
@@ -34,10 +36,12 @@ object BJGui extends SimpleSwingApplication {
     val txtStake = txtField
     txtStake.enabled = false
     val bnNewRound = new Button {
-      text = "New Round!"
+      text = "New Round"
+      background = java.awt.Color.darkGray
+      foreground = java.awt.Color.white
     }
     val lblMoney = new Label {
-      text = "Money: " + round.humanRoundPlayer.player.money.toString
+      text = "Money :" + round.humanRoundPlayer.player.money.toString
     }
     val lblCenterDefault = new Label {
       icon = new ImageIcon(getClass.getResource("blackjack.png"))
@@ -60,10 +64,11 @@ object BJGui extends SimpleSwingApplication {
       c.gridy = 1
       c.ipadx= 75
       layout(bnStand) = c
+
+      background = java.awt.Color.white
     }
 
     val gridBagPanelW = new GridBagPanel {
-
       val c = new Constraints
 
       c.gridx = 0
@@ -83,10 +88,17 @@ object BJGui extends SimpleSwingApplication {
       c.gridx = 0
       c.gridy = 3
       layout(lblMoney) = c
+
+      background = java.awt.Color.white
     }
 
-    val flowPanelS = new FlowPanel
-    val flowPanelN = new FlowPanel
+    val flowPanelS = new FlowPanel {
+      background = java.awt.Color.white
+    }
+    val flowPanelN = new FlowPanel {
+      background = java.awt.Color.white
+    }
+
     // setze primäres Layout
     contents = new BorderPanel {
       layout(gridBagPanelW) = West
@@ -94,6 +106,7 @@ object BJGui extends SimpleSwingApplication {
       layout(flowPanelN) = North
       layout(flowPanelS) = South
       layout(gridBagPanelE) = East
+      background = java.awt.Color.white
     }
 
     // setze Listener
@@ -106,7 +119,7 @@ object BJGui extends SimpleSwingApplication {
     reactions += {
       case ButtonClicked(component) if component == bnGiveCard =>
         round = GameCoreController.hit(round)
-        updateCards
+        updateCards(flowPanelS, round.humanRoundPlayer.hand.visibleCards)
 
       case ButtonClicked(component) if component == bnStand =>
         bnGiveCard.enabled = false
@@ -114,18 +127,16 @@ object BJGui extends SimpleSwingApplication {
         bnNewRound.enabled = true
 
         // ermittle den Gewinner und verändere dessen Geld + verändere Center Bild
-        // TODO show dealer cards
         round = GameCoreController.finish(round)
         if (round.humanRoundPlayer.winner) {
           lblCenterDefault.icon = new ImageIcon(getClass.getResource("winner.jpg"))
         } else {
           lblCenterDefault.icon = new ImageIcon(getClass.getResource("lost.jpg"))
         }
-        // TODO get right amount of money after round
-        lblMoney.text = "Money: " + round.humanRoundPlayer.player.money.toString
-
+        lblMoney.text = "Money :" + round.humanRoundPlayer.player.money.toString
+        updateCards(flowPanelN, round.bankRoundPlayer.hand.allCards)
         // starte neue Runde
-        round = GameCoreController.startNewRound(game)
+        round = GameCoreController.startNewRound(round.game)
 
       case ButtonClicked(component) if component == bnNewRound =>
         startNewRound
@@ -135,10 +146,12 @@ object BJGui extends SimpleSwingApplication {
           Dialog.showMessage(new FlowPanel, "Please use numbers only!")
           txtStake.text = ""
         } else {
-          round = GameCoreController.bet(round,(txtStake.text).toInt)
           txtStake.enabled = false
           bnGiveCard.enabled = true
           bnStand.enabled = true
+          round = GameCoreController.bet(round,(txtStake.text).toInt)
+          updateCards(flowPanelS, round.humanRoundPlayer.hand.visibleCards)
+          updateCards(flowPanelN, round.bankRoundPlayer.hand.visibleCards)
         }
     }
 
@@ -150,62 +163,52 @@ object BJGui extends SimpleSwingApplication {
       txtStake.enabled = true
       txtStake.text = ""
       bnNewRound.enabled = false
-      updateCards
+      flowPanelN.contents.clear
+      flowPanelS.contents.clear
     }
 
-    def updateCards = {
-      def getScaledImageLabel(card: Card):Label = {
+    def getScaledImageLabel(card: Card):Label = {
 
-        def getColor(color:String):String = color match {
-          case "Kreuz" => "clubs"
-          case "Pik" => "spade"
-          case "Herz" => "heart"
-          case "Karo" => "diamond"
-          case "unknown" => "back"
-        }
-
-        def getNumber(number:String): String = number match {
-          case "A" => "ace"
-          case "J" => "jack"
-          case "Q" => "queen"
-          case "K" => "king"
-          case "unknown" => ""
-          case _ => number
-        }
-
-        // Image Größe ändern und als Icon setzen
-        val imageFilePath:String = getColor(card.color) + "_" + getNumber(card.number) + ".png"
-        val ic = new ImageIcon(getClass.getResource(imageFilePath))
-        // TODO in future: relative size to screen size
-        val img:Image = ic.getImage.getScaledInstance(150,-1,0)
-        ic.setImage(img)
-
-        val label = new Label {
-          icon = ic
-        }
-
-        return label
+      def getColor(color:String):String = color match {
+        case "Kreuz" => "clubs"
+        case "Pik" => "spade"
+        case "Herz" => "heart"
+        case "Karo" => "diamond"
+        case "unknown" => "back"
       }
 
-      // fülle den Spielerpanel mit seinen Karten
-      val cards:List[Card] = round.humanRoundPlayer.hand.visibleCards
-      flowPanelS.contents.clear()
+      def getNumber(number:String): String = number match {
+        case "A" => "ace"
+        case "J" => "jack"
+        case "Q" => "queen"
+        case "K" => "king"
+        case "unknown" => ""
+        case _ => number
+      }
 
+      // Image Größe ändern und als Icon setzen
+      val imageFilePath:String = getColor(card.color) + "_" + getNumber(card.number) + ".png"
+      val ic = new ImageIcon(getClass.getResource(imageFilePath))
+      // TODO in future: relative size to screen size
+      val img:Image = ic.getImage.getScaledInstance(150,-1,0)
+      ic.setImage(img)
+      val label = new Label {
+        icon = ic
+      }
+
+      return label
+    }
+
+    def updateCards(flowPanel:FlowPanel, cards:List[Card] ) : FlowPanel = {
+      flowPanel.contents.clear()
       for (card<-cards) {
-        flowPanelS.contents += getScaledImageLabel(card)
+        flowPanel.contents += getScaledImageLabel(card)
       }
-      flowPanelS.revalidate()
-      flowPanelS.repaint()
+      flowPanel.revalidate()
+      flowPanel.repaint()
 
-      // fülle den Dealerpanel mit seinen Karten
-      val cardsDealer:List[Card] = round.bankRoundPlayer.hand.visibleCards
-      flowPanelN.contents.clear()
-
-      for (card<-cardsDealer) {
-        flowPanelN.contents += getScaledImageLabel(card)
-      }
-      flowPanelN.revalidate()
-      flowPanelN.repaint()
+      return flowPanel
     }
+
   }
 } 
